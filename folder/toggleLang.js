e1f17c25f4802abdbef1945b262602e8b51acdfc
WanakitsamphanTh <121081364+WanakitsamphanTh@users.noreply.github.com>
2024-11-24 15:06:56 +0900

    Delete folder directory

--- folder/toggleLang.js
+++
@@ -1,8 +0,0 @@
-// Example JavaScript to toggle active language
-document.querySelectorAll('.header-lang a').forEach((link) => {
-  link.addEventListener('click', (event) => {
-    event.preventDefault();
-    document.querySelectorAll('.header-lang a').forEach((l) => l.classList.remove('active'));
-    event.target.classList.add('active');
-  });
-});
